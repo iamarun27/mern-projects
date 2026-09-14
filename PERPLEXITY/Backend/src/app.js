db234fa49ba101257ce65da75eecda174643c983
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-09-14 13:55:35 +0530

    initialize frontend with React,Redux and tailwind css

--- PERPLEXITY/Backend/src/app.js
+++ PERPLEXITY/Backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
@@ -8,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods:['GET',"POST","DELETE"]
+  }),
+);
 
 // health check
 
