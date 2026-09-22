407b78c7faa19b50a4ad00a0849670ea905e06dc
Arun Kumar <arunkumaryadav2705@gmail.com>
2026-09-22 11:43:10 +0530

    initialize langgraph-battle-ai-arena

---
+++ AI_ARENA/src/config/config.ts
@@ -0,0 +1,25 @@
+import {config} from 'dotenv'
+config()
+
+// gemini api-key
+// mistral api key
+// cohere api-key
+
+type CONFIG= {
+  readonly GOOGLE_API_KEY:string;
+  readonly MISTRAL_API_KET:string;
+  readonly COHERE_API_KEY:string
+}
+
+
+const config: CONFIG = {
+GOOGLE_API_KEY: process.env.GOOGLE_API_KEY ||"",
+MISTRAL_API_KEY: process.env.MISTRAL_API_KEY ||"",
+COHERE_API_KEY: process.env.COHERE_API_KEY ||"",
+}
+
+
+export default config
+
+
+
